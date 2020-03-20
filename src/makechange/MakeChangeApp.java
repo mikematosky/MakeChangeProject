157353570fc7279b1680106c429a06adfccaec2e
mikematosky <mikematosky@hotmail.com>
2020-03-20 16:40:45 -0600

    Created RandomTotals

--- src/makechange/MakeChangeApp.java
+++ src/makechange/MakeChangeApp.java
@@ -1,10 +1,32 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("$"+getTotal());
 
+	}
+	
+	public static String getChange() {
+		String changeString="";
+		
+		return changeString;
+	}
+	public static double giveMoney() {
+		double handedOver=0;
+		Scanner kb= new Scanner(System.in);
+		
+		kb.close();
+		return handedOver;
+	}
+	public static double getTotal() {
+		double total=(Math.random()*100);
+		int allPennies= (int)(total*100);
+		double roundedCost= allPennies/100.00;
+		
+		return roundedCost;
 	}
 
 }
