5a868fef0a3b6530666b963922428cd598906ff8
mikematosky <mikematosky@hotmail.com>
2020-03-20 16:48:31 -0600

    Global Scanner

--- src/makechange/MakeChangeApp.java
+++ src/makechange/MakeChangeApp.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class MakeChangeApp {
 	
+	static Scanner kb= new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		System.out.println("$"+getTotal());
-
+		
+		
+		kb.close();
 	}
 	
 	public static String getChange() {
@@ -16,15 +20,22 @@ public class MakeChangeApp {
 	}
 	public static double giveMoney() {
 		double handedOver=0;
-		Scanner kb= new Scanner(System.in);
 		
-		kb.close();
 		return handedOver;
 	}
 	public static double getTotal() {
 		double total=(Math.random()*100);
 		int allPennies= (int)(total*100);
 		double roundedCost= allPennies/100.00;
+		/*
+		 * I am fully aware that this loses precision. This is just to create the total
+		 * that pops up on the cash register.... So this is an 'Office Space' rounding 
+		 * error that only the programmers know about and is theoretically as beneficial
+		 * as it is harmful to the profit... I will just allow the total to be inputed in future
+		 * versions. So this is just my secret. But it's better than just truncating if you ask me.
+		 */
+		
+		
 		
 		return roundedCost;
 	}
