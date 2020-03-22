61a2cebbf4eba54f2b3dde03a745de2a297a42f4
mikematosky <mikematosky@hotmail.com>
2020-03-22 16:26:55 -0600

    NegErrorCheck

--- src/makechange/MakeChangeApp.java
+++ src/makechange/MakeChangeApp.java
@@ -7,6 +7,11 @@ public class MakeChangeApp {
 	static Scanner kb= new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		
+		greeting();
+		kb.hasNextLine();
+		registerSay("Enter the bill total: $???.??");
+		
 		System.out.println("$"+getTotal());
 		
 		
@@ -24,9 +29,9 @@ public class MakeChangeApp {
 		return handedOver;
 	}
 	public static double getTotal() {
-		double total=(Math.random()*100);
-		int allPennies= (int)(total*100);
-		double roundedCost= allPennies/100.00;
+		//double total=(Math.random()*100);
+		//int allPennies= (int)(total*100);
+		//double roundedCost= allPennies/100.00;
 		/*
 		 * I am fully aware that this loses precision. This is just to create the total
 		 * that pops up on the cash register.... So this is an 'Office Space' rounding 
@@ -34,10 +39,115 @@ public class MakeChangeApp {
 		 * as it is harmful to the profit... I will just allow the total to be inputed in future
 		 * versions. So this is just my secret. But it's better than just truncating if you ask me.
 		 */
+		//return roundedCost;
+
+		registerSay("Enter the bill total: $???.??");
+		System.out.print("Your Total:  $");
+		double total= kb.nextDouble();
 		
 		
-		
-		return roundedCost;
+		//Check to make sure the double is positive
+		while(!isPositive(total)) {
+				registerSay ("The input is negative!", 
+						"Try again!");
+				System.out.println();
+				total= kb.nextDouble();
+		}
+		return total;
 	}
+	
+	public static void greeting() {
+		System.out.println(" =========================================");
+		System.out.println("||                                       ||");
+		System.out.println("|| WELCOME TO THIS QUARANTINE-ENHANCED   ||");
+		System.out.println("||                                       ||");
+		System.out.println("||       CASH REGISTER PROGRAM!          ||");
+		System.out.println("||                                       ||");
+		System.out.println(" =========================================");
+	}
+	
+	
+	public static void registerSay(String sayWhat) {
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println(" =========================================");
+		System.out.println("||                                       ||");
+		System.out.printf("||    %28s      ||%n",sayWhat);
+		System.out.println("||                                       ||");
+		System.out.println("||                                       ||");
+		System.out.println("||                                       ||");
+		System.out.println(" =========================================");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+	}
+	
+	public static void registerSay(String firstLine, String secondLine) {
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println(" =========================================");
+		System.out.println("||                                       ||");
+		System.out.printf("||    %-28s       ||%n",firstLine);
+		System.out.println("||                                       ||");
+		System.out.printf("||        %-20s           ||%n", secondLine);
+		System.out.println("||                                       ||");
+		System.out.println(" =========================================");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+	
+	
+	//For error checking the input
+	public static boolean isPositive(double total) {
+		return (total>0);
+	}
+	
+	public final static void clearConsole()
+	{
+	    try
+	    {
+	        final String os = System.getProperty("os.name");
+
+	        if (os.contains("Windows"))
+	        {
+	            Runtime.getRuntime().exec("cls");
+	        }
+	        else
+	        {
+	            Runtime.getRuntime().exec("clear");
+	        }
+	    }
+	    catch (final Exception e)
+	    {
+	        //  Handle any exceptions.
+	    }
+	}
+	
+//	public static double convertToMoney(double nonMoney) {
+//		
+//	}
 
 }
