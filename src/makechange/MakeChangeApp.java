b7edc91efe2b0faef0cc1f4b41632369999f03df
mikematosky <mikematosky@hotmail.com>
2020-03-22 18:25:55 -0600

    Whole lotta refactoring

--- src/makechange/MakeChangeApp.java
+++ src/makechange/MakeChangeApp.java
@@ -6,6 +6,7 @@ public class MakeChangeApp {
 	
 	static Scanner kb= new Scanner(System.in);
 	private static double total;
+	private static double received;
 	
 	public static void main(String[] args) {
 		
@@ -18,74 +19,62 @@ public class MakeChangeApp {
 		kb.close();
 	}
 	
-	public static String getChange() {
-		String changeString="";
-		
-		return changeString;
-	}
-	public static double giveMoney() {
-		double handedOver=0;
-		
-		return handedOver;
-	}
-	
 	
 	public static void getTotal() {
 		
 		//FOR TESTING
 		//double total=(Math.random()*100);
 
-		registerSay("Enter the bill total: $???.??");
+		System.out.println("Enter the bill total: $???.??");
 		System.out.print("Your Total:  $");
-		total= kb.nextDouble();		
-		//Check to make sure the double is positive
-		while(!isPositive(total)) {
-			registerSay ("The input is negative!", 
-					"Try again!");
-			System.out.print("Your Total:  $");
-			total= kb.nextDouble();
-		}				
-		registerSay("Your Total is: $"+toMoney(total));		
+		//Convert the input to Money right away
+		total= toMoney(kb.nextDouble());		
+		
+						
+		System.out.println("Your Total is: $"+total);
+		kb.nextLine();
 	}
 	
 	public static void takeMoney() {
-		double moneyReceived;
-		registerSay("How much did they", "give you? ");
-		System.out.println("They gave me: $");
-		moneyReceived= kb.nextDouble();
+		boolean isEnough= false;
+		System.out.println("How much did they give you? ");
+		System.out.print("They gave me: $");
+		received= toMoney(kb.nextDouble());
 		
-		if(moneyReceived < total) {
-			registerSay("That is not enough!",
-					"Try Again!");
-			kb.nextDouble();
+		while(!isEnough) {
+			if(received < total) {
+				System.out.println("That is not enough! Needs to be > $"+total);
+				System.out.println("They gave me: $");
+				received= toMoney(kb.nextDouble());
+			}
+			else {
+				isEnough=true;
+			}
 		}
+		System.out.println("GREAT! We need $"+toMoney(total-received));
+		
 		
 	}
-	
 
-	
-
-	
-	public static int getDollars(double total) {
-		return (int)total;
-		
-	}
-	public static int getPennies(double total) {
-		return (int)(((total+0.005)*100)%100);
-	}
-	public static boolean isPositive(double total) {
-		return (total > 0);
-	}
 	public static long getTotalPennies(double total) {
+		
 		return (long)total*100;
 	}
+	
 	
 	//ROUND Strange Gas Station decimals from inputs
 	public static double toMoney(double num) {
 		int pennies;
 		double money;
-		num+=0.005;
+		 
+		//recursive
+		if(num < 0) {
+			System.out.println("Needs to be a Positive Number! Try again");
+			System.out.print("New money value: $");
+			num= toMoney(kb.nextDouble());
+		}
 		
+		num+=0.005;
 		pennies= (int)(num * 100);
 		money= pennies/100.00;	
 	
@@ -102,59 +91,6 @@ public class MakeChangeApp {
 		System.out.println("||       CASH REGISTER PROGRAM!          ||");
 		System.out.println("||                                       ||");
 		System.out.println(" =========================================");
-	}
-	
-	
-	public static void registerSay(String sayWhat) {
-		
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println(" =========================================");
-		System.out.println("||                                       ||");
-		System.out.printf("||    %28s      ||%n",sayWhat);
-		System.out.println("||                                       ||");
-		System.out.println("||                                       ||");
-		System.out.println("||                                       ||");
-		System.out.println(" =========================================");
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		
-	}
-	
-	//Two Lines Overload for registerSay
-	public static void registerSay(String firstLine, String secondLine) {
-		
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println(" =========================================");
-		System.out.println("||                                       ||");
-		System.out.printf("||    %-28s       ||%n",firstLine);
-		System.out.println("||                                       ||");
-		System.out.printf("||        %-20s           ||%n", secondLine);
-		System.out.println("||                                       ||");
-		System.out.println(" =========================================");
-		System.out.println();
-		System.out.println();
-		System.out.println();
 	}
 	
 }
